2de639fded20e3afdc81fd369642971b60d04fdb
ding404 <ding404@gmail.com>
2017-12-10 16:32:06 +0800

    1. upgrade all elpa plugins

---
+++ elpa/imenu-list-20171120.1255/imenu-list-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "imenu-list" "20171120.1255" "Show imenu entries in a separate buffer" '((cl-lib "0.5")) :stars '(#("77" 0 2 (font-lock-face paradox-star-face))) :commit "62de9b897f0f6168f57cc64532dbcaa1bfcbe47e" :url "https://github.com/bmag/imenu-list")
