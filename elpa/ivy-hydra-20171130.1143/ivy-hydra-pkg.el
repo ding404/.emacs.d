a58ed3c0d090bd3f4d711db01e2d0e25bc019f4b
ding jiong <ding404@gmail.com>
2018-04-14 16:48:40 +0800

    1. update all elpa plugins

--- elpa/ivy-hydra-20171130.1143/ivy-hydra-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ivy-hydra" "20171130.1143" "Additional key bindings for Ivy" '((emacs "24.1") (ivy "0.9.0") (hydra "0.13.4")) :stars '(#("650" 0 3 (font-lock-face paradox-star-face))) :commit "b960aa4fa52b19dba4d5994fb29f445164209f46" :url "https://github.com/abo-abo/swiper" :keywords '("completion" "matching" "bindings"))
