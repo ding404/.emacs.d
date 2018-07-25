15b6ea00e96ac541342dde48e555588be563346c
ding jiong <ding404@gmail.com>
2018-07-26 07:35:39 +0800

    1. add indent offset to 2 for html/css/js

--- init.el
+++ init.el
@@ -332,5 +332,8 @@ https://github.com/jaypei/emacs-neotree/pull/110"
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
 
 ;;; init.el ends here
